a5adb7ebcb3b597962b3f6388aae4a90bcce03c2
smiriti24 <smiritimittal24@gmail.com>
2017-04-07 05:18:33 +0530

    Add files via upload

---
+++ Day4/ClearMyArrayList.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+ 
+public class ClearMyArrayList {
+ 
+    public static void main(String a[]){
+        ArrayList<String> arrl = new ArrayList<String>();
+        //adding elements to the end
+        arrl.add("First");
+        arrl.add("Second");
+        arrl.add("Third");
+        arrl.add("Random");
+        System.out.println("Actual ArrayList:"+arrl);
+        arrl.clear();
+        System.out.println("After clear ArrayList:"+arrl);
+    }
+}
---
+++ Day4/MyArrayListClone.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+ 
+public class MyArrayListClone {
+ 
+    public static void main(String a[]){
+        ArrayList<String> arrl = new ArrayList<String>();
+        //adding elements to the end
+        arrl.add("First");
+        arrl.add("Second");
+        arrl.add("Third");
+        arrl.add("Random");
+        System.out.println("Actual ArrayList:"+arrl);
+        ArrayList<String> copy = (ArrayList<String>) arrl.clone();
+        System.out.println("Cloned ArrayList:"+copy);
+    }
+}
